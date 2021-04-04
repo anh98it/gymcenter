5d9fbacd041d3c1ced5849409face7da9b1c4060
Nguyen Anh <anh98it@gmail.com>
2021-04-04 08:51:36 +0700

    unit project

---
+++ src/main/java/com/anhnguyen/gymcenter/GymcenterApplication.java
@@ -0,0 +1,13 @@
+package com.anhnguyen.gymcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymcenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anhnguyen/gymcenter/GymcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anhnguyen.gymcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
